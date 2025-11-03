7bb61242dd6235c56209b82deec0e94ce5c8a81b
NikolasSpirouGH <nispyrou.se@gmail.com>
2025-11-03 16:23:57 +0200

    test

--- src/main/java/com/cloud_ml_app_thesis/entity/CustomAlgorithmConfiguration.java
+++ src/main/java/com/cloud_ml_app_thesis/entity/CustomAlgorithmConfiguration.java
@@ -4,6 +4,8 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,6 +23,7 @@ public class CustomAlgorithmConfiguration {
 
     @ManyToOne
     @JoinColumn(name = "algorithm_id")
+    @OnDelete(action = OnDeleteAction.CASCADE)
     private CustomAlgorithm algorithm;
 
     @OneToMany(mappedBy = "configuration", cascade = CascadeType.ALL, orphanRemoval = true)
--- src/main/java/com/cloud_ml_app_thesis/repository/CustomAlgorithmConfigurationRepository.java
+++ src/main/java/com/cloud_ml_app_thesis/repository/CustomAlgorithmConfigurationRepository.java
@@ -1,9 +1,13 @@
 package com.cloud_ml_app_thesis.repository;
 
+import com.cloud_ml_app_thesis.entity.CustomAlgorithm;
 import com.cloud_ml_app_thesis.entity.CustomAlgorithmConfiguration;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CustomAlgorithmConfigurationRepository extends JpaRepository<CustomAlgorithmConfiguration, Integer> {
+    List<CustomAlgorithmConfiguration> findByAlgorithm(CustomAlgorithm algorithm);
 }
